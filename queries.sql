b51a25dffe1e65521ed03632b8874e812aa5b1ab
wuyy2001 <wuyy2001@students.cs.ubc.ca>
2023-08-02 21:39:20 -0700

    Added selection queries

---
+++ queries.sql
@@ -0,0 +1,22 @@
+-- Groups
+SELECT * 
+FROM Groups
+
+-- PartOfActor
+-- arg (groupNm)
+SELECT StageName
+FROM PartOfActor
+WHERE GroupName = groupNm
+
+-- Visitor
+-- arg (ticketNum)
+SELECT VisitorName 
+FROM Visitor
+WHERE TicketNumber = ticketNum
+
+-- Staff
+-- arg (ID)
+SELECT StaffName
+FROM Staff
+WHERE StaffID = ID
+
